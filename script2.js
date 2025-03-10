c2d5d267cece9f43d6f1b7479c13cb2d826eb376
NeverEatBreakfast-V2 <Otella15@outlook.com>
2025-03-10 11:35:11 -0400

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,20 @@
+
+16151.47.0 (Official Build) stable-channel dedede
+Firmware Version
+Google_Magolor.13606.667.0
+Customization ID
+magolor
+ARC
+13017017 SDK Version: 33
+JavaScript
+V8 13.3.415.22
+User Agent
+Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36
+Command Line
+/opt/google/chrome/chrome --enable-native-gpu-memory-buffers --disable-features=VideoDecodeBatching --enable-webgl-image-chromium --enable-features=Pepper3DImageChromium,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,UmaStorageDimensions,ArcAdbSideloading,ArcEnableVirtioBlkMultipleWorkers,Crostini,Borealis,SmartDim,OnDeviceSpeechRecognition --gpu-sandbox-failures-fatal=yes --gpu-sandbox-start-early --video-capture-use-gpu-memory-buffer --enable-logging --log-level=1 --use-cras --enable-wayland-server --user-data-dir=/home/chronos --arc-build-properties={'device': 'dedede_cheets', 'firstApiLevel': '28'} --child-wallpaper-large=/usr/share/chromeos-assets/wallpaper/child_large.jpg --child-wallpaper-small=/usr/share/chromeos-assets/wallpaper/child_small.jpg --default-wallpaper-large=/usr/share/chromeos-assets/wallpaper/default_large.jpg --default-wallpaper-small=/usr/share/chromeos-assets/wallpaper/default_small.jpg --enable-touchview --guest-wallpaper-large=/usr/share/chromeos-assets/wallpaper/guest_large.jpg --guest-wallpaper-small=/usr/share/chromeos-assets/wallpaper/guest_small.jpg --disable-mojo-broker --ash-use-cros-mojo-service-manager --cros-healthd-uses-service-manager --login-profile=user --has-chromeos-keyboard --form-factor=CONVERTIBLE --guest-wallpaper-large=/usr/share/chromeos-assets/wallpaper/guest_large.jpg --guest-wallpaper-small=/usr/share/chromeos-assets/wallpaper/guest_small.jpg --child-wallpaper-large=/usr/share/chromeos-assets/wallpaper/child_large.jpg --child-wallpaper-small=/usr/share/chromeos-assets/wallpaper/child_small.jpg --default-wallpaper-large=/usr/share/chromeos-assets/wallpaper/default_large.jpg --default-wallpaper-small=/usr/share/chromeos-assets/wallpaper/default_small.jpg --device-help-content-id=magolor --ozone-nnpalm-properties={} --arc-availability=officially-supported --enable-arcvm --arc-erofs --enable-houdini --enable-houdini64 --enterprise-enrollment-initial-modulus=15 --enterprise-enrollment-modulus-limit=19 --ml_service=enabled --ondevice_handwriting=use_rootfs --ondevice_document_scanner=use_rootfs --feature-management-level=0 --feature-management-max-level=2 --feature-management-scope=0 --login-manager --enable-features=ArcAdbSideloading,ArcEnableVirtioBlkMultipleWorkers,Borealis,Crostini,DefaultANGLEVulkan,OnDeviceSpeechRecognition,Pepper3DImageChromium,SmartDim,UmaStorageDimensions,Vulkan,VulkanFromANGLE --disable-features=VideoDecodeBatching --flag-switches-begin --flag-switches-end
+Executable Path
+/opt/google/chrome/chrome
+Profile Path
+/home/chronos/u-70c85af86e366451633a84eee20d67bbd9b7718a
+Command-line Variations
+eyJkaXNhYmxlLWZlYXR1cmVzIjoiQXVkaW9JbnB1dENvbmZpcm1SZWFkc1Zp..
